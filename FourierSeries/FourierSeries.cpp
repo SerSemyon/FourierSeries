7d0c2cb87cd395b65c74ba32112f5272fbed1bb8
Семен Серов <sersemyon@gmail.com>
2023-04-25 23:36:18 +0300

    Вейвлеты

--- FourierSeries/FourierSeries.cpp
+++ FourierSeries/FourierSeries.cpp
@@ -547,14 +547,191 @@ void FiltrTask4()
     std::cout << "];" << std::endl;
 }
 
+template <typename Iterator>
+std::vector<std::complex<double>> Fft(Iterator begin, Iterator end, bool invert = false) {
+    std::vector<std::complex<double>> x(begin, end);
+    if (x.size() != 1 && std::fmod(std::log2(x.size()), 1) != 0) {
+        throw std::invalid_argument("Размерность сигнала должна быть степенью двойки ");
+    }
+
+    size_t n = x.size();
+    if (n == 1)  return x;
+
+    std::vector<std::complex<double>> x1(n / 2), x2(n / 2);
+    for (size_t i = 0, j = 0; i < n; i += 2, ++j) {
+        x1[j] = x[i];
+        x2[j] = x[i + 1];
+    }
+    x1 = Fft(x1.begin(), x1.end(), invert);
+    x2 = Fft(x2.begin(), x2.end(), invert);
+
+    double theta = 2 * M_PI / n * (invert ? -1 : 1);
+    std::complex<double> w(1), wn(std::cos(theta), std::sin(theta));
+    for (size_t i = 0; i < n / 2; ++i) {
+        x[i] = x1[i] + w * x2[i];
+        x[i + n / 2] = x1[i] - w * x2[i];
+        if (invert) {
+            x[i] /= 2, x[i + n / 2] /= 2;
+        }
+        w *= wn;
+    }
+    return x;
+}
+
+void WaveTask1() {
+    std::cout << "WaveTask1" << std::endl;
+    size_t size = 64;
+    double h = 0.2;
+    std::vector<double> x(size);
+    for (size_t i = 0; i < size; i++)
+    {
+        x[i] = h * i;
+    }
+    std::vector<double> y(size);
+    for (size_t i = 0; i < size; i++) {
+        y[i] = std::atan(2 * x[i] - 6.5) * x[i] + x[i] * std::sin(x[i] + std::cos(2 * x[i]));
+    }
+    for (size_t i = 0; i < y.size(); ++i) {
+        std::cout << x[i] << ',' << y[i] << '\n';
+    }
+    std::cout << "\n\n";
+    size_t size_window = 32;
+    std::vector<std::vector<std::complex<double>>> vec_X;
+    vec_X.reserve(std::ceil(size / size_window) + 1);
+    for (auto beg = x.begin(), end = x.begin() + size_window; end < x.end(); ++beg, ++end) {
+
+        auto beg_y = y.begin() + std::distance(x.begin(), beg);
+        auto end_y = beg_y + size_window;
+        vec_X.push_back(Fft(beg_y, end_y));
+    }
+    for (size_t i = 0; i < vec_X.size(); ++i) {
+        for (size_t j = 0; j < vec_X[i].size(); ++j) {
+            std::cout << i << ',' << j << ',' << std::abs(vec_X[i][j]) << '\n';
+        }
+    }
+    std::cout << std::endl << std::endl;
+}
+
+double Haar(double x) {
+    if (x < 1 && x >= 0.5) {
+        return -1;
+    }
+    else if (x > 0 && x < 0.5) {
+        return 1;
+    }
+    return 0;
+}
+
+double MHAT(double x) {
+    return (1 - x * x) * std::exp(-(x * x) / 2.0);
+}
+
+void ExampleWaveletMHAT() {
+    std::cout << "ExampleWaveletMHAT" << std::endl;
+    size_t size = 64;
+    double b0 = 1, a0 = 0.5;
+    double h = 0.2;
+    std::vector<double> x(size);
+    for (size_t i = 0; i < size; i++)
+    {
+        x[i] = h * i;
+    }
+    std::vector<double> mhat(size);
+    std::vector<double> y(size);
+
+    auto psi = [b0, a0](double x, size_t m, size_t n) {
+        return (1.0 / std::sqrt(m)) * MHAT((x - n) / m);
+    };
+
+    for (size_t i = 0; i < size; ++i) {
+        mhat[i] = MHAT(x[i]);
+        y[i] = std::sin(2 * x[i]) + std::sin(3 * x[i]);
+    }
+    std::vector<std::vector<double>> W(x.size(), std::vector<double>(x.size(), 0));
+    for (size_t i = 0; i < mhat.size(); ++i) {
+        std::cout << x[i] << ',' << mhat[i] << '\n';
+    }
+    std::cout << "\n\n";
+    for (size_t i = 0; i < y.size(); ++i) {
+        std::cout << x[i] << ',' << y[i] << '\n';
+    }
+    std::cout << "\n\n";
+    for (size_t m = 0; m < x.size(); ++m) {
+        for (size_t n = 0; n < x.size(); ++n) {
+            double scalar_product = 0.0;
+            // вычисляем скалярное произведение
+            for (size_t i = 0; i < x.size(); ++i) {
+                scalar_product += y[i] * psi(i, m, n);
+            }
+            W[m][n] = scalar_product;
+        }
+    }
+    for (size_t i = 0; i < W.size(); ++i) {
+        for (size_t j = 0; j < W[i].size(); ++j) {
+            std::cout << i << ',' << j << ',' << W[i][j] << '\n';
+        }
+    }
+    std::cout << "\n\n";
+}
+
+void ExampleWaveletHAAR() {
+    std::cout << "ExampleWaveletHAAR" << std::endl;
+    size_t size = 64;
+    double b0 = 1, a0 = 0.5;
+    double h = 0.2;
+    std::vector<double> x(size);
+    for (size_t i = 0; i < size; i++)
+    {
+        x[i] = h * i;
+    }
+    std::vector<double> mhat(size);
+    std::vector<double> y(size);
+
+    auto psi = [b0, a0](double x, size_t m, size_t n) {
+        return (1.0 / std::sqrt(m)) * Haar((x - n) / m);
+    };
+
+    for (size_t i = 0; i < size; ++i) {
+        mhat[i] = Haar(x[i]);
+        y[i] = std::sin(2 * x[i]) + std::sin(3 * x[i]);
+    }
+    std::vector<std::vector<double>> W(x.size(), std::vector<double>(x.size(), 0));
+    for (size_t i = 0; i < mhat.size(); ++i) {
+        std::cout << x[i] << ',' << mhat[i] << '\n';
+    }
+    std::cout << "\n\n";
+    for (size_t i = 0; i < y.size(); ++i) {
+        std::cout << x[i] << ',' << y[i] << '\n';
+    }
+    std::cout << "\n\n";
+    for (size_t m = 0; m < x.size(); ++m) {
+        for (size_t n = 0; n < x.size(); ++n) {
+            double scalar_product = 0.0;
+            // вычисляем скалярное произведение
+            for (size_t i = 0; i < x.size(); ++i) {
+                scalar_product += y[i] * psi(i, m, n);
+            }
+            W[m][n] = scalar_product;
+        }
+    }
+    for (size_t i = 0; i < W.size(); ++i) {
+        for (size_t j = 0; j < W[i].size(); ++j) {
+            std::cout << i << ',' << j << ',' << W[i][j] << '\n';
+        }
+    }
+    std::cout << "\n\n";
+}
+
 int main()
 {
 
     //Test();
     //Task4();
     ////Task5();
-    FiltrTask1();
+    /*FiltrTask1();
     FiltrTask2();
     FiltrTask3();
-    FiltrTask4();
+    FiltrTask4();*/
+
+    ExampleWaveletHAAR();
 }
